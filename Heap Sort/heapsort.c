4fa66116a441d862bb54214d759ef527e724b291
Davidsksilva <davidsimonkastle@gmail.com>
2017-08-30 22:48:39 -0300

    Update heapsort.c

--- Heap Sort/heapsort.c
+++ Heap Sort/heapsort.c
@@ -5,23 +5,23 @@ void createHeap(int arr[],int i, int h)
 	int aux = arr[i];
 	int j= i * 2 +1;
 	
-	while(j <= h)
+	while(j <= h) // Enquanto o indice do filho não passar do indice máximo
 	{
-		if(j < h)
+		if(j < h) // Tem filho?
 		{
-			if(arr[j] < arr[j+1])
-				j=j+1;
+			if(arr[j] < arr[j+1]) // Esse filho é o maior dos 2?
+				j=j+1; // O outro filho é maior
 		}
-		if(aux < arr[j])
+		if(aux < arr[j]) // Pai é menor que o maior filho?
 		{
-			arr[i] = arr[j];
-			i=j;
+			arr[i] = arr[j]; // Filho vira pai
+			i=j; // Analizar os filhos do novo pai, como alterou, ver se está balanceado tambem
 			j=2*i+1;
 		}
 		else
-			j=h+1;
+			j=h+1; // Sair do while
 	}
-	arr[i]= aux;
+	arr[i]= aux; // Antigo pai ocupa lugar do ultimo filho analisado
 }
 void heapSort(int arr[],int n)
 {
